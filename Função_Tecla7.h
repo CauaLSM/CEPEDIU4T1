076224f1ed639820400024ee44afbc2140e67ca7
Erick de sousa <erickvestas@gmail.com>
2025-01-17 19:26:01 -0300

    Finalização do codigo

---
+++ Função_Tecla7.h
@@ -0,0 +1,27 @@
+// Função tecla 7
+// Piscar o LED vermelho 5 vezes lentamente.
+void piscar_led_vermelho(){
+
+gpio_put(LedVermelho,1); // Liga led vermelho
+sleep_ms(1000); // tempo de 1 segundos aceso
+gpio_put(LedVermelho,0); // Desliga Led vermelho
+sleep_ms(1000); // tempo de 1 segundos apagado
+gpio_put(LedVermelho,1); // Liga led vermelho
+sleep_ms(1000); // tempo de 1 segundos aceso
+gpio_put(LedVermelho,0); // Desliga Led vermelho
+sleep_ms(1000); // tempo de 1 segundos apagado
+gpio_put(LedVermelho,1); // Liga led vermelho
+sleep_ms(1000); // tempo de 1 segundos aceso
+gpio_put(LedVermelho,0); // Desliga Led vermelho
+sleep_ms(1000); // tempo de 1 segundos apagado
+gpio_put(LedVermelho,1); // Liga led vermelho
+sleep_ms(1000); // tempo de 1 segundos aceso
+gpio_put(LedVermelho,0); // Desliga Led vermelho
+sleep_ms(1000); // tempo de 1 segundos apagado
+gpio_put(LedVermelho,1); // Liga led vermelho
+sleep_ms(1000); // tempo de 1 segundos aceso
+gpio_put(LedVermelho,0); // Desliga Led vermelho
+sleep_ms(1000); // tempo de 1 segundos apagado
+
+}
+
