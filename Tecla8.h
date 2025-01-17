60f4d43ee43802d49cd332a3caf51e436edc563c
Erick de sousa <erickvestas@gmail.com>
2025-01-17 20:51:53 -0300

    Correção do codigo

--- Tecla8.h
+++ Tecla8.h
@@ -3,7 +3,9 @@
 void Tecla8(){
 
 gpio_put(LedAzul,1); // Liga led azul
+sleep_ms(2000); // tempo de 2 segundos aceso
+gpio_put(LedAzul,0); // desliga led azull
 sleep_ms(1000); // tempo de 1 segundos aceso
-gpio_put(LedAzul,0); // desliga led azul
+
 
 }
