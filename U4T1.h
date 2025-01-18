1d3c33277404fb65d1d7531c2c3066dfe0a1b06f
Kleber Marçal <kleber.sm@gmail.com>
2025-01-17 22:44:28 -0300

    Ajustes da tecla3 e buzzer com PWM

--- U4T1.h
+++ U4T1.h
@@ -1,6 +1,8 @@
 #ifndef __U4T1_H_
 #define __U4T1_H_
 
+#include <hardware/pwm.h>
+
 #include "functions/functions.h"
 #include "keypad/keypad.h"
 
@@ -8,6 +10,6 @@
 #define LED_AZUL        12
 #define LED_VERMELHO    13
 
-#define BUZZER          21
+#define BUZZER          10
 
 #endif
