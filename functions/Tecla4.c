92cbb7829688b9a5368f50a1793e42308d786119
Raiane Damascena <rai.paixao2012@gmail.com>
2025-01-18 19:45:32 -0300

    Inclusão de biblioteca

--- functions/Tecla4.c
+++ functions/Tecla4.c
@@ -1,7 +1,9 @@
 // Função Tecla 4 
 
-void liga_led_azul() {
-    gpio_put(LedAzul,1); // Liga led azul
+#include "functions.h"
+
+void Tecla4() {
+    gpio_put(LED_AZUL,1); // Liga led azul
      sleep_ms(5000); // tempo de 5 segundos aceso
-    gpio_put(LedAzul,0); // Desliga Led azul
+    gpio_put(LED_AZUL,0); // Desliga Led azul
 }
--- functions/Tecla6.c
+++ functions/Tecla6.c
@@ -1,9 +1,10 @@
-// Função Tecla 6
-void Alterna_led_verde() {
+#include "functions.h"
+
+void Tecla6() {
     while (true) { // Laço infinito para alternar o LED verde
-        gpio_put(LedVerde, 1); // Liga LED verde
+        gpio_put(LED_VERDE, 1); // Liga LED verde
         sleep_ms(1000);         // Espera 1 segundo
-        gpio_put(LedVerde, 0); // Desliga LED verde
+        gpio_put(LED_VERDE, 0); // Desliga LED verde
         sleep_ms(1000);         // Espera 1 segundo
     }
 }
