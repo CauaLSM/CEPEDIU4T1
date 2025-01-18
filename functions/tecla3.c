3730e488b822e7aa4179489cc3ac71d461414bdb
Kleber Marçal <kleber.sm@gmail.com>
2025-01-17 21:26:49 -0300

    Teclas 3 e 5

--- functions/tecla3.c
+++ functions/tecla3.c
@@ -1,3 +1,7 @@
+#include "functions.h"
+
 void tecla3() {
-  printf("Tecla 3");
+  gpio_put(LED_AZUL, true);
+  sleep_ms(2000);
+  gpio_put(LED_AZUL, false);
 }
--- functions/tecla5.c
+++ functions/tecla5.c
@@ -1,3 +1,7 @@
+#include "functions.h"
+
 void tecla5() {
-  printf("tecla5");
+  gpio_put(BUZZER, true);
+  sleep_ms(2000);
+  gpio_put(BUZZER, false);
 }
