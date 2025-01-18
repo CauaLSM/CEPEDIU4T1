8c75377666acf4db05ac0bd2a9a4363cd895344a
Raiane Damascena <rai.paixao2012@gmail.com>
2025-01-18 15:17:40 -0300

    Arquivos copiados para pasta functions

--- Tecla4.c
+++
@@ -1,7 +0,0 @@
-// Função Tecla 4 
-
-void liga_led_vermelho() {
-    gpio_put(LedVermelho,1); // Liga led vermelho
-     sleep_ms(5000); // tempo de 5 segundos aceso
-    gpio_put(LedVermelho,0); // Desliga Led vermelho
-}
