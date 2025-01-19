68bbde3e3d9057b63b0c769a60625d812986385a
isla-creator <islasilva736@gmail.com>
2025-01-19 12:41:48 -0300

    Create TeclaD.c
    Entrar em um modo especial onde os LEDs alternam em sequência e o buzzer toca um tom contínuo até que outra tecla seja pressionada.

---
+++ functions/TeclaD.c
@@ -0,0 +1,43 @@
+// Ligar leds alternadamente
+#include "function.h"
+
+void teclaD() {
+int Q =2;
+while (Q<=2, Q--) {
+gpio_put(LED_VERDE, 1);
+gpio_put(LED_AZUL, 0);
+gpio_put(LED_VERMELHO, 0);
+sleep_ms(500); 
+
+gpio_put(LED_VERDE, 0);
+gpio_put(LED_AZUL, 1);
+sleep_ms(500);
+
+gpio_put(LED_AZUL, 0);
+gpio_put(LED_VERMELHO, 1);
+sleep_ms(500); 
+
+gpio_put(LED_VERDE, 1);
+gpio_put(LED_AZUL, 1);
+gpio_put(LED_VERMELHO, 0);
+sleep_ms(500); 
+
+gpio_put(LED_VERDE, 0);
+gpio_put(LED_AZUL, 1);
+gpio_put(LED_VERMELHO, 1);
+sleep_ms(500); 
+
+gpio_put(LED_VERDE, 1);
+gpio_put(LED_AZUL, 0);
+gpio_put(LED_VERMELHO, 1);
+sleep_ms(500);
+
+gpio_put(LED_VERDE, 1);
+gpio_put(LED_AZUL, 1);
+gpio_put(LED_VERMELHO, 1);
+sleep_ms(500); 
+
+gpio_put(LED_VERDE, 0);
+gpio_put(LED_AZUL, 0);
+gpio_put(LED_VERMELHO, 0); }
+sleep_ms(500); }
